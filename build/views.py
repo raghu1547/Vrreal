bf8cac88b0e84237bcde32a1b4553e4ccf6dfeaf
Krishna Sai <krishna31082001@gmail.com>
2020-09-25 01:01:08 +0530

    Added Pagination - Krishna

--- build/views.py
+++ build/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
 from .models import Prop
 # Create your views here.
@@ -9,20 +9,21 @@ class SiteList(ListView):
     template_name = 'build/build_list.html'
     """
 
+
 def search(request):
-    prop=Prop.objects.order_by('-date_added')
-    #keywords
-    #city
-    if 'city'in request.GET:
-        city=request.GET['city']
+    prop = Prop.objects.order_by('-date_added')
+    # keywords
+    # city
+    if 'city' in request.GET:
+        city = request.GET['city']
         if city:
-            prop=prop.filter(city__iexact=city)
-    #state
-    if 'state'in request.GET:
-        state=request.GET['state']
+            prop = prop.filter(city__iexact=city)
+    # state
+    if 'state' in request.GET:
+        state = request.GET['state']
         if state:
-            prop=prop.filter(state__iexact=state)
-    #bedrooms
+            prop = prop.filter(state__iexact=state)
+    # bedrooms
     """
     if 'bedrooms'in request.GET:
         bedrooms=request.GET['bedrooms']
@@ -34,10 +35,15 @@ def search(request):
         if price:
             listings=listings.filter(price__lte=price)
     """
-    context={'prop_list':prop,
-        'request':request.GET
-    }
-    print(context['request'])
-    return render(request,'build/build_list.html',context)
-
-
+    pageCursor = Paginator(prop, 10)
+    page = request.GET.get("page")
+    try:
+        prop_list = pageCursor.page(page)
+    except PageNotAnInteger:
+        prop_list = pageCursor.page(1)
+    except EmptyPage:
+        prop_list = pageCursor.page(pageCursor.num_pages)
+    context = {'prop_list': prop_list,
+               }
+    # print(context['request'])
+    return render(request, 'build/build_list.html', context)
